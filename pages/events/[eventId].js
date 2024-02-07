fa92c3959c8f243e59de89d7209da927fc1b0124
anjali-sim <anjalighetia123@gmail.com>
2024-02-07 11:58:27 +0530

    :hammer: Added the event details component with its styling

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,28 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
-    return <div>
-        <h1>Event Detail</h1>
-    </div>
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>no Event Found!</p>;
+  }
+
+  return (
+    <>
+        <EventSummary title={event.title} />
+        <EventLogistics date={event.date} address={event.address} image={event.image} imageAlt={event.title} />
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
+    </>
+  );
 }
 
 export default EventDetailPage;
