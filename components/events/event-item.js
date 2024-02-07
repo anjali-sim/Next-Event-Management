faf44d718c535f35cd1a7d1cd967fc136f1066c1
anjali-sim <anjalighetia123@gmail.com>
2024-02-07 11:13:22 +0530

    :lipstick: Added the style for the event items and list

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,32 +1,33 @@
 import Link from "next/link";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
-    const {title, image, date, location, id} = props;
+  const { title, image, date, location, id } = props;
 
-    const humanReadableDate = new Date(date).toLocaleDateString('en-us', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    });
+  const humanReadableDate = new Date(date).toLocaleDateString("en-us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const formattedAddress = location.replace(', ', '\n');
-    const exploreLink = `/events/${id}`;
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
 
   return (
-    <li>
-      <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-            <Link href={exploreLink}> Explore Event</Link>
+        <div className={classes.actions}>
+          <Link href={exploreLink}> Explore Event</Link>
         </div>
       </div>
     </li>
